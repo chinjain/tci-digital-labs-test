2fc1cdefa7916535b3a5355e7f421e26166a7754
chinmay jain <chinmayjain03@gmail.com>
2024-05-01 12:03:50 +0530

    First commit

---
+++ src/main/java/com/tci/coding/test/TciDigitalLabsTest/TciDigitalLabsTestApplication.java
@@ -0,0 +1,13 @@
+package com.tci.coding.test.TciDigitalLabsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TciDigitalLabsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TciDigitalLabsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tci/coding/test/TciDigitalLabsTest/TciDigitalLabsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tci.coding.test.TciDigitalLabsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TciDigitalLabsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
